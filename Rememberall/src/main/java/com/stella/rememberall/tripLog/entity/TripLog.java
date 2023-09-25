f25ae594a6540b549c5d37456288cb56424f9832
mingadinga <minpearl0826@gmail.com>
2023-09-25 13:17:52 +0900

    feat:>create TripLog Entity with Validation Test/#6

---
+++ Rememberall/src/main/java/com/stella/rememberall/tripLog/entity/TripLog.java
@@ -0,0 +1,71 @@
+package com.stella.rememberall.tripLog.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "trip_log")
+@Entity
+public class TripLog extends BaseTimeEntity {
+    @Id
+    @Column(name = "trip_log_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min=1)
+    private String title;
+
+    @Column(name = "trip_start_date")
+    private LocalDate tripStartDate;
+
+    @Column(name = "trip_end_date")
+    private LocalDate tripEndDate;
+
+    @Column(name = "user_id")
+    @NotNull
+    private final Long userId;
+
+    @Builder
+    public TripLog(final String title, final LocalDate tripStartDate, final LocalDate tripEndDate, final Long userId){
+        this.title = titleIfNotEmpty(title);
+        this.tripStartDate = startDateIfEarlierThanEndDate(tripStartDate, tripEndDate);
+        this.tripEndDate = endDateIfLaterThanEndDate(tripStartDate, tripEndDate);
+        this.userId = userIdIfNotNull(userId);
+    }
+
+    private Long userIdIfNotNull(Long userId) {
+        if (userId != null) return userId;
+        throw new IllegalArgumentException("userId은 null일 수 없습니다.");
+    }
+
+    private static LocalDate startDateIfEarlierThanEndDate(LocalDate startDate, LocalDate endDate) {
+        checkDatesAreNotNull(startDate, endDate);
+        if (startDate.isBefore(endDate) || startDate.isEqual(endDate)) return startDate;
+        throw new IllegalArgumentException(String.format("startDate는 endDate보다 늦을 수 없습니다. startDate : %s, endDate : %s", startDate.toString(), endDate.toString()));
+    }
+
+    private static LocalDate endDateIfLaterThanEndDate(LocalDate startDate, LocalDate endDate) {
+        checkDatesAreNotNull(startDate, endDate);
+        if (endDate.isAfter(startDate) || endDate.isEqual(startDate)) return endDate;
+        throw new IllegalArgumentException(String.format("endDate는 startDate보다 빠를 수 없습니다. startDate : %s, endDate : %s", startDate.toString(), endDate.toString()));
+    }
+
+    private static void checkDatesAreNotNull(LocalDate startDate, LocalDate endDate) {
+        if (startDate == null || endDate == null)
+            throw new IllegalArgumentException("startDate 혹은 endDate는 null일 수 없습니다.");
+    }
+
+    private static String titleIfNotEmpty(String title) {
+        if (title != null && !title.isEmpty()) return title;
+        throw new IllegalArgumentException("title은 빈 값일 수 없습니다.");
+    }
+
+}
---
+++ Rememberall/src/test/java/com/stella/rememberall/tripLog/entity/TripLogTest.java
@@ -0,0 +1,113 @@
+package com.stella.rememberall.tripLog.entity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TripLogTest {
+
+    @Test
+    public void 제목은_한글자_이상이다() {
+        TripLog tripLog = TripLog.builder()
+                .title("제목은 한 글자 이상이다")
+                .tripStartDate(LocalDate.of(2023, 9, 15))
+                .tripEndDate(LocalDate.of(2023, 9, 18))
+                .userId(1L)
+                .build();
+
+        assertTrue(!tripLog.getTitle().isEmpty());
+    }
+
+    @Test
+    public void 제목은_빈값일수_없다() {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            TripLog emptyTitleTripLog = TripLog.builder()
+                    .title("")
+                    .tripStartDate(LocalDate.of(2023, 9, 15))
+                    .tripEndDate(LocalDate.of(2023, 9, 18))
+                    .userId(1L)
+                    .build();
+        });
+
+        assertTrue(e.getMessage().contains("title은 빈 값일 수 없습니다."));
+    }
+
+    @Test
+    public void 시작날짜는_null이면_안된다() {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            TripLog tripLog = TripLog.builder()
+                    .title("제목")
+                    .tripStartDate(null)
+                    .tripEndDate(LocalDate.of(2023, 9, 18))
+                    .userId(1L)
+                    .build();
+        });
+
+        assertTrue(e.getMessage().contains("startDate 혹은 endDate는 null일 수 없습니다."));
+    }
+
+    @Test
+    public void 종료날짜는_null이면_안된다() {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            TripLog tripLog = TripLog.builder()
+                    .title("제목")
+                    .tripStartDate(LocalDate.of(2023, 9, 15))
+                    .tripEndDate(null)
+                    .userId(1L)
+                    .build();
+        });
+
+        assertTrue(e.getMessage().contains("startDate 혹은 endDate는 null일 수 없습니다."));
+    }
+
+    @Test
+    public void 시작날짜는_종료날짜보다_이르다() {
+        TripLog tripLog = TripLog.builder()
+                .title("제목")
+                .tripStartDate(LocalDate.of(2023, 9, 15))
+                .tripEndDate(LocalDate.of(2023, 9, 15))
+                .userId(1L)
+                .build();
+
+        assertThat(tripLog.getTripStartDate())
+                .isBeforeOrEqualTo(tripLog.getTripEndDate());
+    }
+
+    @Test
+    public void 시작날짜는_종료날짜보다_늦으면_안된다() {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            TripLog tripLog = TripLog.builder()
+                    .title("제목")
+                    .tripStartDate(LocalDate.of(2023, 9, 15))
+                    .tripEndDate(LocalDate.of(2023, 8, 1))
+                    .userId(1L)
+                    .build();
+        });
+
+        assertTrue(e.getMessage().contains("startDate는 endDate보다 늦을 수 없습니다."));
+    }
+
+    @Test
+    public void 유저아이디는_null이면_안된다() {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            TripLog tripLog = TripLog.builder()
+                    .title("제목")
+                    .tripStartDate(LocalDate.of(2023, 9, 15))
+                    .tripEndDate(LocalDate.of(2023, 9, 18))
+                    .userId(null)
+                    .build();
+        });
+
+        assertTrue(e.getMessage().contains("userId은 null일 수 없습니다."));
+    }
+
+    // @TODO : Basetime Entity 검사는 JPA 통합 테스트에서
+
+
+}
